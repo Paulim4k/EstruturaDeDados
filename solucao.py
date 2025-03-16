4bd36017f4236c34a9d6c90140bfc92ec7814504
Paulim4k <107083817+Paulim4k@users.noreply.github.com>
2025-03-16 12:08:14 -0300

    Create solucao.py

---
+++ solucao.py
@@ -0,0 +1,13 @@
+from collections import doque
+
+# EXemplo de fila em Python
+fila = daque()
+
+# Adicionando tarefas
+fila.append("Tarefa 1")
+fila.append("Tarefa 2")
+fila.append("Tarefa 3")
+
+# Processando tarefas 
+While fila:
+    print(f"Processando: {fila.popletf()}")
